d301b3e34709cf59b6076466d6c46ada974fc7b6
erina.kamata <xx1p2k00280406@gmail.com>
2020-10-09 14:49:43 +0900

    Add Employee show

---
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -0,0 +1,49 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EmployeesShowServlet
+ */
+@WebServlet("/employees/show")
+public class EmployeesShowServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesShowServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        //em.find は主キーを検索条件にして、データを1件取得するためのメソッド
+        //Employee.classに対応しているテーブルからデータを取得
+        //Employee.classに対応しているテーブルの主キーは idのため、idの値が第二引数に指定されている値 Integer.parseInt(request.getParameter("id")) と一致するデータを取得
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("employee", e);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+        rd.forward(request, response);
+    }
+
+}
